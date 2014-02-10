abd238329780312b24b9b340beadd8f95c7b52d0
corysmith11 <csmith@rycoleinvest.com>
2014-02-10 00:41:15 -0700

    updated images path

--- main.rb
+++ main.rb
@@ -26,6 +26,28 @@ helpers do
   end
 end
 
+def card_image(card)
+  suit = case card[0]
+  when 'H' then 'Hearts'
+  when 'D' then 'Diamonds'
+  when 'C' then 'Clubs'
+  when 'S' then 'Spades'
+end
+
+value = card[1]
+if ['J', 'Q', 'K', 'A'].include?(value)
+  value = case card[1]
+    when 'J' then 'Jack'
+    when 'Q' then 'Queen'
+    when 'K' then 'King'
+    when 'A' then 'Ace'
+  end
+end
+
+
+"<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
 before do
   @show_hit_or_stay_buttons = true
 end
@@ -44,12 +66,18 @@ get '/welcome' do
 end
 
 post '/welcome' do
+  if params[:name].empty?
+    @error = "Name is required"
+    halt erb(:form)
+  end
+
   session[:name] = params[:name]
   redirect '/game'
 end
 
+
 get '/game' do
-  suits =  ['Hearts','Diamonds','Clubs','Spades']
+  suits =  ['H','D','C','S']
   cards =   ['2','3','4','5','6','7','8','9','10','Jack','queen','king','ace']
   session[:deck] = suits.product(cards).shuffle!
 
@@ -60,14 +88,18 @@ get '/game' do
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
 
-
   erb :game
 end
 
 post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
-  if calc_total(session[:player_cards]) > 21
-    @error = "Sorry you busted!!"
+
+  player_total = calc_total(session[:player_cards])
+  if player_total == 21
+    @success = "Congrats #{session[:name]} you hit Blackjack!"
+    @show_hit_or_stay_buttons = false
+  elsif player_total > 21
+    @error = "Sorry #{session[:name]} busted!!"
     @show_hit_or_stay_buttons = false
   end
 
@@ -75,7 +107,7 @@ post '/game/player/hit' do
 end
 
 post '/game/player/stay' do
-@success = "You chosen to stay"
+@success = "#{session[:name]} has chosen to stay."
 @show_hit_or_stay_buttons = false
   erb :game
 end
